6792641cbbbd596101122ef5201c97a17cbc76a0
unknown <crsn1111@gmail.com>
2022-03-25 16:26:32 +0900

    알고리즘

--- 31~40/32.ts
+++ 31~40/32.ts
@@ -1,3 +1,6 @@
-const n = prompt().split(' ')
-console.log(n.length)
+function t32(n:string) {
+  const r = n.split(" ")
+  console.log(r.length)
+}
 
+t32("안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.")
--- 31~40/33.ts
+++ 31~40/33.ts
@@ -1,3 +1,7 @@
-const sum = (a: number, b: number) => {
-  return a + b;
-};
+function t33(n : string) {
+  let r = n.split(" ").reverse().join(" ");
+  console.log(r);
+}
+
+t33("1 2 3 4 5")
+
